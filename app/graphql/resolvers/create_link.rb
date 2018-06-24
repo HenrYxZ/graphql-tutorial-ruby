d10cdcb92ce277b61589fbeb7298028a83604ff7
Hernaldo Henriquez <hjhenriq@gmail.com>
2018-06-24 16:24:18 -0400

    error handling - add error message for model validation

--- app/graphql/resolvers/create_link.rb
+++ app/graphql/resolvers/create_link.rb
@@ -1,23 +1,20 @@
-module Resolvers
-  # Create Link resolver
-  class CreateLink < GraphQL::Function
-    # arguments passed as "args"
-    argument :description, !types.String
-    argument :url, !types.String
+class Resolvers::CreateLink < GraphQL::Function
+  argument :description, !types.String
+  argument :url, !types.String
 
-    # return type from the mutation
-    type Types::LinkType
+  type Types::LinkType
 
-    # the mutation method
-    # _obj - is parent object, which in this case is nil
-    # args - are the arguments passed
-    # _ctx - is the GraphQL context (which would be discussed later)
-    def call(_obj, args, ctx)
-      Link.create!(
-        description: args[:description],
-        url: args[:url],
-        user: ctx[:current_user]
-      )
-    end
+  def call(_obj, args, ctx)
+    Link.create!(
+      description: args[:description],
+      url: args[:url],
+      user: ctx[:current_user]
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    # this would catch all validation errors and translate them to
+    # GraphQL::ExecutionError
+    GraphQL::ExecutionError.new(
+      "Invalid input: #{e.record.errors.full_messages.join(', ')}"
+    )
   end
 end
